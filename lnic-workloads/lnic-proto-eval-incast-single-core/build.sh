ee151848fb030aac13ae1928fd6d90d5023d5073
Serhat Arslan <sarslan@stanford.edu>
2021-03-29 19:49:46 +0000

    Fixed the symlink for proto-eval-incast workload

--- lnic-workloads/lnic-proto-eval-incast-single-core/build.sh
+++ lnic-workloads/lnic-proto-eval-incast-single-core/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ln -s ../../../../tests-lnic/lnic-incast-single-core.riscv lnic-proto-eval-incast-single-core.riscv
+ln -s ../../../../tests-lnic/lnic-proto-eval-incast-single-core.riscv lnic-proto-eval-incast-single-core.riscv
